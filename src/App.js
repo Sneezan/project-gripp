864f1ce2dde3cf2f76574dfce45f945e9094736d
Sneezan <ceciliafraanlund@gmail.com>
2023-01-20 15:01:40 +0100

    filestructure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { MainPage } from 'components/main/Main.js';
 import { About } from 'components/about/About';
 import { NotFound } from 'components/NotFound/NotFound';
 import { WhatIsThis } from 'components/whatisthis/WhatIsThis';
-import { WhereToPlay } from 'components/about/playhere/WhereToPlay';
+import { WhereToPlay } from 'components/playhere/WhereToPlay';
 import { Contact } from 'components/contact/Contact';
 
 export const App = () => {
